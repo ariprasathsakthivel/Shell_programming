bb8cf17f319246c43cfd1c1c7094ed4ba185a371
unknown <ariprasath3@gmail.com>
2021-06-05 20:02:29 +0530

    repetition completed

--- repetition/forloop/prime_factors.sh
+++ repetition/forloop/prime_factors.sh
@@ -1,13 +1,21 @@
 #! /bin/bash -x
 read -p "Enter a postive number" n;
-for ((i=2; i<$n; i++))
+for ((i=2; $i<=$n; i++))
 do
-	for ((j=1; j<$i; j++))
-	do
-		if [ $(($i%$j)) != 0 ];
+	if [ $(($n%$i)) -eq 0 ];
+	then
+		flag=1;
+		for ((j=2; $j<$i; j++))
+		do
+			if [ $(($i%$j)) -eq 0 ];
+			then
+				flag=0;
+				break;
+			fi
+		done
+		if [ $flag -eq 1 ];
 		then
-			$n=$(($n/$i));
-			echo "prime factors are $i,"
+			echo "$i";
 		fi
-	done
+	fi
 done
