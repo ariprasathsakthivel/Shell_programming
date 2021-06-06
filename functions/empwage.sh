1cdfc383851f0038c967f2dd79881155da37565f
unknown <ariprasath3@gmail.com>
2021-06-06 17:56:42 +0530

    functions completed and updated

---
+++ functions/empwage.sh
@@ -0,0 +1,28 @@
+#! /bin/bash -x
+full_time=1;
+part_time=2;
+emp_rate_per_hr=20;
+num_of_working_days=10;
+max_hrs_in_month=50;
+#variable
+total_emp_hrs=0;
+total_working_days=0;
+function getworkinghours() {
+	case $1 in
+		$full_time) emp_hrs=8
+		;;
+		$part_time) emp_hrs=4
+		;;
+		*) emp_hrs=0
+		;;
+	esac
+	echo $emp_hrs;
+}
+
+while [ $total_emp_hrs -lt $max_hrs_in_month -a $total_working_days -lt $num_of_working_days ]
+do
+	total_working_days=$(($total_working_days+1));
+	work_hrs="$( getworkinghours $((RANDOM%3)) )";
+	total_emp_hrs=$(($total_emp_hrs+$work_hrs));
+done
+wage=$(($total_emp_hrs*$emp_rate_per_hr));
--- functions/primepalindrome.sh
+++ functions/primepalindrome.sh
@@ -9,4 +9,43 @@ function prime(){
 			flag=0;
 		fi
 	done
+	echo "$flag";
 }
+
+function palindrome(){
+	flag=0
+	n=$1
+	while [ $n -ne 0 ]
+	do
+		temp_1=$(($n%10));
+		temp_2=$(($n-temp_1));
+		n=$((temp_2/10));
+		if [ $flag -eq 0 ];
+		then
+			temp_3=$(($temp_1*10));
+			flag=1;
+		elif [ $flag -eq 1 ];
+		then
+			temp_3=$(((($temp_3+$temp_1))*10));
+		fi
+	done
+	echo "$(($temp_3/10))";
+
+}
+
+read -p "Enter a number" n;
+if [ "$( prime $n )" -eq 0 ];
+then
+	echo "$n is not a prime number";
+elif [ "$( prime $n )" -eq 1 ];
+then
+	echo "$n is a prime number";
+	pal_no="$( palindrome $n )"
+	if [ "$( prime $pal_no )" -eq 0 ];
+	then
+		echo "$n's palindrome is not a prime number";
+	elif [ "$( prime $pal_no )" -eq 1 ];
+	then
+		echo "$n's palindrome is a prime number";
+	fi
+fi
