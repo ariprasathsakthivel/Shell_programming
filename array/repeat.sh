0a1134946a27406f7e433e40bbd3c885b47ef09c
unknown <ariprasath3@gmail.com>
2021-06-08 21:32:21 +0530

    updated code in repeat in array

--- array/repeat.sh
+++ array/repeat.sh
@@ -1,12 +1,38 @@
 #! /bin/bash -x
-i=0;
-j=2
-x=11;
-while [ $x -le 100 ]
+function palindrome(){
+	flag=0
+	n=$1
+	while [ $n -ne 0 ]
+	do
+		temp_1=$(($n%10));
+		temp_2=$(($n-temp_1));
+		n=$((temp_2/10));
+		if [ $flag -eq 0 ];
+		then
+			temp_3=$(($temp_1*10));
+			flag=1;
+		elif [ $flag -eq 1 ];
+		then
+			temp_3=$(((($temp_3+$temp_1))*10));
+		fi
+	done
+	echo "$(($temp_3/10))";
+
+}
+
+
+count=0;
+for (( val=0; val<=100;val++ ))
 do
-	repeat[i]=$x;
-	x=$((11*$j));
-	j=$(($j+1));
-	i=$(($i+1));
+	if [ $val -ge 10 ];
+	then
+		a=$( palindrome $val );
+		if [ $a -eq $val ];
+		then
+			array[count]=$a;
+			count=$(($count+1));
+		fi
+	fi
 done
-echo "${repeat[@]}";
+
+echo "${array[@]}"
